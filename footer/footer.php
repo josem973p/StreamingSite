40af49bd3acfc4e6913e0a5cada19237cd85bbfd
manuel <josscas1997@gmail.com>
2022-11-13 16:23:11 -0600

    PROYECT : Inicial ->  Commit inicial,  creacioon de vista home

---
+++ footer/footer.php
@@ -0,0 +1,15 @@
+
+
+
+<div data-role="footer"  style="
+  background-color:#663366;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  color: white;
+">
+		<p style="font-family:helvetica;font-size:12px;text-align:center;">
+			&copy;2022 Autobúses de Oriente ADO, S.A. de C.V. Todos los derechos reservados. Aviso Legal.
+		</p>
+</div>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style=heigth:auto>
+
+<?php
+			require_once('header/header.php');
+?>
+
+
+
+<div style="width:100%;     position: absolute; top:80px; display:flex; justify-content:center">
+<?php
+			require_once('home/home.php');
+		?>
+</div>
+<div">
+		
+        <?php
+			require_once('footer/footer.php');
+		?>
+</div>
+</body>
+</html>
